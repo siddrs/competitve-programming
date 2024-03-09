c6aa390bc19ee54b87ac964a0431b07d28aec75b
Siddharth S <siddharthrshete03@gmail.com>
2024-03-09 21:49:12 +0530

    [solved] drinks

--- 0800-200B-drinks/main.cpp
+++ 0800-200B-drinks/main.cpp
@@ -1,11 +1,25 @@
 #include<bits/stdc++.h>
 
-using namespace std;
-
 int main(){
 
-    cout << "Hello World" << endl;
+    int n;
+    std::cin >> n;
 
+    double p[n];
+    for (int i = 0; i < n; i++) {
+        std::cin >> p[i];
+    }
+    
+    double totalOrangeJuice;
+    double orangeJuice; 
+
+
+    for (int j = 0; j < n; j++){
+        orangeJuice = ((p[j])/100);
+        totalOrangeJuice += orangeJuice;
+    }
+    double output = (( totalOrangeJuice / ((double)n) ) * 100);
+    std::cout << output;
 
     return 0;
 }
